20cb46dc9f08a6dc75809a558c7471ddcbffe93a
reyhanrafaidhil1126 <reyhanrafaidhil1126@gmail.com>
2025-03-03 14:35:46 +0000

    tampilan awal DONE sisa membawa informasi ke arah form selanjutnya (after awal)

---
+++ lib/models/data.dart
@@ -0,0 +1,17 @@
+class Data {
+  int ? id;
+  String ? nama;
+  String ? gambar;
+  String ? deskripsi;
+  int ? jumlah;
+  int ? harga;
+
+  Data({
+    this.id,
+    this.nama,
+    this.gambar,
+    this.deskripsi,
+    this.jumlah,
+    this.harga,
+  });
+}
---
+++ lib/screens/awal.dart
@@ -0,0 +1,72 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:nested/models/data.dart';
+import 'package:faker/faker.dart' as faker;
+
+
+class awal extends StatelessWidget{
+  final List<Data> mydata = List.generate(
+    20, 
+    (index){
+      return Data(
+        id: 1 + Random().nextInt(20),
+        nama: faker.faker.person.name(),
+        gambar: "https://picsum.photos/id/${index}/200/300",
+        jumlah: 100000 + Random().nextInt(20000),
+        harga: 5000000 + Random().nextInt(10000),
+        deskripsi: faker.faker.lorem.sentence(),
+        );
+      }
+    );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Nested",
+          style: TextStyle(
+            color: Colors.white
+          ),
+          ),
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            child: ListTile(
+              leading: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    image: NetworkImage("${mydata[index].gambar}"),
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center
+                    )
+                ),
+              ),
+              title: Text("${mydata[index].nama}"),
+              subtitle: Text("${mydata[index].deskripsi}"),
+              trailing: Container(
+                child: Column(
+                  children: [
+                    Container(
+                      child: Text(" Jumlah :${mydata[index].jumlah}"),
+                    ),
+                    Container(
+                      child: Text("Harga :${mydata[index].harga}"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
